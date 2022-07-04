95bbf8e01ef8afac549a39909de049f4610f39b4
sogiita <105888878+sogiita@users.noreply.github.com>
2022-07-04 20:54:25 +0100

    dawn1

--- userconfig.js
+++ userconfig.js
@@ -5,7 +5,7 @@ const CONFIG = new Config({
     refreshIn: 10
   },
   temperature: {
-    location: 'London',
+    location: 'Brent, London',
     scale: 'C'
   },
   clock: 'h:i p',
@@ -18,10 +18,10 @@ const CONFIG = new Config({
       w: ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia']
     }
   },
-  disabled: ['todo-list'] // crypto-rate 
+  disabled: ['todo-list'] // crypto-rate
+});
   tabs: [
     {
-      name: 'boards',
       background_url: 'src/img/banners/bg-1.png',
       categories: [{
         name: 'links',
@@ -31,7 +31,7 @@ const CONFIG = new Config({
             name: 'proton'
           },
           {
-            name: 'heavens above',
+             name: 'heavens above',
             url: 'https://heavens-above.com/',
           },
           {
@@ -39,12 +39,18 @@ const CONFIG = new Config({
             url: 'https://github.com/',
           },
           {
+            name: '/v/',
+            url: 'https://4chan.org/v/',
+            icon: 'device-gamepad',
+            icon_color: '#8b647b'
+          },
+          {
             name: 'nix',
             url: 'https://www.reddit.com/r/unixporn/',
           },
           {
-            name: '/w/',
-            url: 'https://boards.4channel.org/w/catalog',
+             name: '/w/',
+            url: 'https://boards.4channel.org/w/catalog'
           }
         ]
       },
@@ -65,8 +71,8 @@ const CONFIG = new Config({
         name: '/a/',
         links: [
           {
-            name: '/g/',
-            url: 'https://4chan.org/g/',
+            name: 'nyaa',
+            url: 'https://nyaa.si/',
           },
           {
             name: 'mangadex',
@@ -79,6 +85,92 @@ const CONFIG = new Config({
           {
             name: 'jump',
             url: 'https://www.viz.com/shonenjump'
-          }
+          }          
         ]
       }
+      ]
+    },
+    {
+      background_url: 'src/img/banners/bg-1.png',
+      categories: [
+        {
+          name: 'music & video',
+          links: [
+            {
+              url: 'https://soundcloud.com/',
+              icon: 'brand-soundcloud',
+              icon_color: '#c57750'
+            },
+            {
+              url: 'https://www.youtube.com/feed/subscriptions',
+              icon: 'brand-youtube',
+              icon_color: '#996767'
+            },
+            {
+              url: 'https://r-a-d.io/',
+              icon: 'radio'
+            },
+          ]
+        },
+        {
+          name: 'socials',
+          links: [
+            {
+              url: 'https://www.instagram.com/',
+              icon: 'brand-instagram',
+              icon_color: 'A8D9EF'
+            },
+            {
+             url: 'https://www.instagram.com/lollysdogwalking/',
+              icon: 'dog-bowl',
+              icon_color: 'A8D9EF'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      background_url: 'src/img/banners/bg-1.png',
+      categories: [
+        {
+          name: 'misc',
+          links: [
+            {
+              name: 'startpages',
+              url: 'https://www.reddit.com/r/startpages/'
+            },
+            {
+              name: 'rainmeter',
+              url: 'https://www.reddit.com/r/Rainmeter/'
+            },
+            {
+              name: 'deviantart',
+              url: 'https://www.deviantart.com/',
+            }
+          ]
+        },
+        {
+          name: 'eorzea',
+          links: [
+            {
+              name: 'eorzeacollection',
+              url: 'https://ffxiv.eorzeacollection.com/glamours',
+            }
+          ]
+        },
+        {
+          name: 'collections',
+          links: [
+            {
+              name: 'mfc',
+              url: 'https://myfigurecollection.net/',
+            },
+            {
+              name: 'mal',
+              url: 'https://myanimelist.net/',
+            }
+          ]
+        }
+      ]
+    }]
+});
